0a91b90a89bfee672e4681d06e1d2d1b53313fd5
war2v <war2v@mtmail.mtsu.edu>
2025-05-15 16:12:54 -0500

    Added construction graphic to accounts page.

--- app/accounts/[id]/page.tsx
+++ app/accounts/[id]/page.tsx
@@ -1,7 +1,13 @@
+import { LuConstruction } from "react-icons/lu";
+
 const AccountPage = () => {
     return ( 
-        <div>
-            Account 
+        <div className="flex justify-center items-center h-full">
+            <div className="bg-neutral-900 p-20 md:p-10 rounded-lg">
+                <h1 className="text-xl justify-center flex items-center gap-x-2"><LuConstruction/>Under Construction</h1>
+                <p className="text-md text-neutral-500">Accounts Page Will be completed soon!</p>
+                <p>Navigate back to <span className="text-purple-400">here</span> for more music.</p>
+            </div> 
         </div>
      );
 }
